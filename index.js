14ee0bf88a9e0224527a6c49ce5eeaba9f0a013d
nishant6217 <me.nishant6217@gmail.com>
2021-05-28 14:21:54 +0530

    set up view enjine

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const port = 8000;
 app.use('/' , require('./routes'))
 
 
+// set up the view enjine,
+app.set('view enjine' , 'ejs');
+app.set('views' ,'./views');
+
+
 
 
 app.listen(port , function(err){
